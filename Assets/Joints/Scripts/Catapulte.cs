8433684750deb7d5a5e3573bb666114881a6d3ef
llontores <ezaugenrikh@yandex.kz>
2024-12-08 01:47:18 +0500

    1

--- Assets/Joints/Scripts/Catapulte.cs
+++ Assets/Joints/Scripts/Catapulte.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Catapulte : MonoBehaviour
 {
     [SerializeField] private float _throwForce = 100f;
     [SerializeField] private Missile _missilePrefab;
     [SerializeField] private Transform _missileSpawnPoint;
+    [SerializeField] private SpringJoint _springJoint;
+    [SerializeField] private float _backThrowForce = 20f;
+    [SerializeField] private float _startSpringForce = 30f;
     
     private Rigidbody _rigidbody;
 
@@ -22,10 +26,17 @@ public class Catapulte : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.E))
         {
             _rigidbody.AddForce(Vector3.up * _throwForce, ForceMode.Impulse);
+            _springJoint.spring = 0;
         }
         else if (Input.GetKeyDown(KeyCode.R))
         {
             Instantiate(_missilePrefab, _missileSpawnPoint.position, Quaternion.identity);
+        }
+        else if (Input.GetKeyDown(KeyCode.X))
+        {
+            _springJoint.spring = _startSpringForce;
+            _rigidbody.AddForce(Vector3.forward * _throwForce, ForceMode.Impulse);
+
         }
     }
 }
